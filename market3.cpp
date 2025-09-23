171f4b9e3a38be7f2d2478d0ef264afa2700f6e5
jadefj17 <jade060107@gmail.com>
2025-09-23 15:17:44 +0900

    Update market3.cpp

--- market3.cpp
+++ market3.cpp
@@ -12,7 +12,7 @@ typedef struct {
 
 Product products[MAX_PRODUCTS];
 int productCount = 0;  
-int soldProducts[MAX_PRODUCTS];  
+int soldProducts[MAX_PRODUCTS]; 
 
 void addProduct() {
     if (productCount >= MAX_PRODUCTS) {
@@ -31,13 +31,12 @@ void addProduct() {
     scanf("%d", &newProduct.quantity);
 
     products[productCount] = newProduct;
-    soldProducts[productCount] = 0; // 초기 판매 수량 0
+    soldProducts[productCount] = 0; 
     productCount++;
 
     printf("상품이 추가되었습니다.\n");
 }
 
-// 상품 판매
 void sellProduct() {
     int id, quantity;
     printf("판매할 상품 ID: ");
@@ -62,26 +61,6 @@ void sellProduct() {
     printf("해당 ID의 상품을 찾을 수 없습니다.\n");
 }
 
-void searchByProductName() {
-    char searchName[50];
-    printf("검색할 상품명: ");
-    scanf("%s", searchName);
-
-    int found = 0;
-    for (int i = 0; i < productCount; i++) {
-        if (strcmp(products[i].name, searchName) == 0) {
-            printf("상품 ID: %d, 상품명: %s, 가격: %.2f, 재고: %d, 판매된 수량: %d\n",
-                products[i].id, products[i].name, products[i].price, products[i].quantity, soldProducts[i]);
-            found = 1;
-            break;
-        }
-    }
-    if (!found) {
-        printf("상품명을 찾을 수 없습니다.\n");
-    }
-}
-
-// 상품 현황
 void viewProductStatus() {
     if (productCount == 0) {
         printf("등록된 상품이 없습니다.\n");
@@ -95,7 +74,6 @@ void viewProductStatus() {
     }
 }
 
-// 메뉴 표시 및 선택
 void menu() {
     int choice;
     while (1) {
@@ -103,7 +81,7 @@ void menu() {
         printf("1. 상품 추가\n");
         printf("2. 상품 판매\n");
         printf("3. 상품 현황\n");
-        printf("4. 상품명 입력 (검색)\n");
+        printf("4. 상품명 입력\n");
         printf("5. 종료\n");
         printf("선택: ");
         scanf("%d", &choice);
@@ -119,7 +97,7 @@ void menu() {
             viewProductStatus();
             break;
         case 4:
-            searchByProductName();  
+            printf("상품명 입력 기능은 아직 구현되지 않았습니다.\n");
             break;
         case 5:
             printf("시스템을 종료합니다.\n");
